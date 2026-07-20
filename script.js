f975724cec76f5aad7156542b94d92925819ce22
Ariyez <33876686+Ariyez@users.noreply.github.com>
2026-07-20 18:01:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -434,7 +434,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
     );
 
     if (valid) {
-      window.location.href = "/Stock/dashboard/dashboard.html";
+      window.location.href = "https://ariyez.github.io/Stock/dashboard/dashboard.html";
     } else {
       message.textContent = "Username atau password salah.";
       message.style.color = "red";
